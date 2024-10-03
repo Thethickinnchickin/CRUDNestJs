ae7b8a443bfb6199ed37960cba6d6be39a949021
Matthew Reiley <59402946+Thethickinnchickin@users.noreply.github.com>
2024-10-03 23:35:36 +0000

    README

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 //import { SeedService } from './seed/seed.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('../server.key'),
+      cert: fs.readFileSync('../server.cert'),
+    },
+  });
   // const seedService = app.get(SeedService);
   // await seedService.populateDatabase();
   // await app.close(); // Close app after seeding
